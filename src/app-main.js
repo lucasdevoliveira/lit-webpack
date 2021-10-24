183371fa464d0dde2a5bc9c310e661fe44ff874c
Lucas de Oliveira <contato@lucasdeoliveira.dev.br>
2021-10-24 00:17:24 -0300

    feature: add layout

--- src/app-main.js
+++ src/app-main.js
@@ -1,10 +1,12 @@
 import { LitElement, html, customElement } from "lit-element";
+import styles from './app-main.styles'
 
 @customElement('app-main')
 export class AppMain extends LitElement {
+    static styles = styles
     render() {
         return html`
-            <h1>Hello word</h1>
+            <h1>Lit-Element</h1>
         `
     }
 }
---
+++ src/app-main.styles.js
@@ -0,0 +1,21 @@
+import { css } from "lit-element"
+
+const styles = [
+    css`
+        :host {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+        h1 {
+            color: #fff;
+            font-size: 32px;
+            font-weight: bold;
+            font-family: 'tahoma';
+            color: #70d829;
+        }
+    `
+]
+
+export default styles
